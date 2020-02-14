aa1001ad47a5e98f4b0456e99a09063d6617e12c
Hamza El Bouatmani <hamza@mon-pc.local>
2020-02-14 20:06:56 +0100

    Add Get Task Test

--- tasks-service/test.py
+++ tasks-service/test.py
@@ -6,3 +6,7 @@ ROOT_URL='http://localhost:5000'
 def test_get_list_of_tasks():
     response = requests.get('{}/tasks/'.format(ROOT_URL))
     assert response.status_code == 200
+
+def test_get_specific_task():
+    response = requests.get('{}/tasks/{}'.format(ROOT_URL, 1))
+    assert response.status_code == 200
