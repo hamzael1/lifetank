9f458b15208c52cab5a7bfdcd9aaaabaf59d692b
Hamza El Bouatmani <hamza@mon-pc.local>
2020-02-14 20:38:56 +0100

    Test Get Single Task

--- tasks-service/test.py
+++ tasks-service/test.py
@@ -6,7 +6,13 @@ ROOT_URL='http://localhost:5000'
 def test_get_list_of_tasks():
     response = requests.get('{}/tasks/'.format(ROOT_URL))
     assert response.status_code == 200
+    assert len(response.json()) > 0
 
 def test_get_specific_task():
-    response = requests.get('{}/tasks/{}'.format(ROOT_URL, 1))
+    response = requests.get('{}/tasks/'.format(ROOT_URL))
+    tasks = response.json()
+    assert len(tasks) > 0
+    random_task_id = tasks[0]['id']
+    response = requests.get('{}/tasks/{}'.format(ROOT_URL, random_task_id))
     assert response.status_code == 200
+    assert response.json()['id'] == random_task_id
