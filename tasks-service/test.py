acd2d6844e83d4bc8b6b816107b4c8e16dfd7441
Hamza El Bouatmani <hamza@mon-pc.local>
2020-02-16 18:49:54 +0100

    Test Patch and Delete Task

--- tasks-service/test.py
+++ tasks-service/test.py
@@ -27,7 +27,7 @@ class TestTasksAPI:
 
     def test_get_list_of_tasks(self):
         """
-            TEST Get all Tasks Request
+            TEST GET Request to get all Tasks in DB
         """
         response = requests.get('{}/tasks/'.format(ROOT_URL))
         assert response.status_code == 200
@@ -39,7 +39,7 @@ class TestTasksAPI:
 
     def test_get_specific_task(self):
         """
-            TEST Get A Specific Task Details
+            TEST Get A Specific Task Details from DB
         """
         with app.app_context():
             random_task = TaskModel.query.all()[0].__dict__
@@ -50,7 +50,7 @@ class TestTasksAPI:
 
     def test_post_new_task(self):
         """
-            TEST POST Request to add a new Task
+            TEST POST Request to add a new Task in DB
         """
         with app.app_context():
             nbr_of_tasks_in_db = len(TaskModel.query.all())
@@ -60,3 +60,28 @@ class TestTasksAPI:
             assert response.status_code == 201, 'Should get a 201 Code Response'
             assert nbr_of_tasks_in_db + 1 == len(TaskModel.query.all()) , 'Expecting number of tasks in DB to increase by one'
             assert response.json()['title'] == 'New Task Test', 'Expecting title of returned object to match title sent in post request'
+    
+    def test_patch_task(self):
+        """
+            TEST PATCH Request to update a Task in DB
+        """
+        with app.app_context():
+            random_task = TaskModel.query.all()[0].__dict__
+            new_title = 'Updated Title of Task'
+            response = requests.patch('{}/tasks/{}/'.format(ROOT_URL, random_task['id']), json={
+                'title': new_title
+            })
+            assert response.status_code == 200, 'Should get a 200 Code Response'
+            updated_task = TaskModel.query.get(random_task['id']).__dict__
+            assert updated_task['title'] == new_title, 'Expected to update the new title in DB'
+
+    def test_delete_task(self):
+        """
+            TEST DELETE Request to delete a Task from DB
+        """
+        with app.app_context():
+            nbr_of_tasks_in_db = len(TaskModel.query.all())
+            random_task = TaskModel.query.all()[0].__dict__
+            response = requests.delete('{}/tasks/{}/'.format(ROOT_URL, random_task['id']))
+            assert response.status_code == 204, 'Should get a 201 Code Response'
+            assert nbr_of_tasks_in_db - 1 == len(TaskModel.query.all()) , 'Expecting number of tasks in DB to decrease by one'
