99c69c73181ff6d9f16be394aac3eabe435471e8
Hamza El <hamza.elbouatmani@gmail.com>
2020-02-19 01:21:59 +0100

    Protect Routes and add user_id support

--- tasks-service/routes.py
+++ tasks-service/routes.py
@@ -16,9 +16,13 @@ class TaskList(Resource):
         current_user = get_jwt_identity()
         tasks = TaskModel.query.filter_by(user_id=current_user['id']).all() # Get Tasks of user
         return task_list_schema.dump(tasks)
+    
+    @jwt_required
     def post(self):
+        current_user = get_jwt_identity()
         new_task = TaskModel(
             title=request.json['title'],
+            user_id=current_user['id']
         )
         db.session.add(new_task)
         db.session.commit()
@@ -26,24 +30,40 @@ class TaskList(Resource):
 
 
 class TaskSingle(Resource):
+    @jwt_required
     def get(self, task_id):
+        current_user = get_jwt_identity()
         task = TaskModel.query.get_or_404(task_id)
-        return task_single_schema.dump(task), '200'
 
+        if task.user_id != current_user['id']:
+            return {'message': 'Unauthorized'}, '403'
+        else:
+            return task_single_schema.dump(task), '200'
+
+    @jwt_required
     def patch(self, task_id):
+        current_user = get_jwt_identity()
         task = TaskModel.query.get_or_404(task_id)
 
-        if 'title' in request.json:
-            task.title = request.json['title']
+        if task.user_id != current_user['id']:
+            return {'message': 'Unauthorized'}, '403'
+        else:
+            if 'title' in request.json:
+                task.title = request.json['title']
 
-        db.session.commit()
-        return task_single_schema.dump(task), '200'
+            db.session.commit()
+            return task_single_schema.dump(task), '200'
 
+    @jwt_required
     def delete(self, task_id):
+        current_user = get_jwt_identity()
         task = TaskModel.query.get_or_404(task_id)
-        db.session.delete(task)
-        db.session.commit()
-        return '', '204'
+        if task.user_id != current_user['id']:
+            return {'message': 'Unauthorized'}, '403'
+        else:
+            db.session.delete(task)
+            db.session.commit()
+            return '', '204'
 
 def init_api(app):
     api = Api(app)
--- tasks-service/test.py
+++ tasks-service/test.py
@@ -1,5 +1,7 @@
 import requests
 import pytest
+from sqlalchemy.sql.expression import func
+
 from app import app
 from model import db, TaskModel
 
@@ -54,78 +56,109 @@ def init_db_for_test_session(request, test_user_1, test_user_2):
             db.drop_all()
     request.addfinalizer(fin)
 
+@pytest.fixture(scope='module', autouse=False)
+def token_user_1(test_user_1):
+    response = requests.post(LOGIN_URL, json={
+            'username': test_user_1['username'],
+            'password': test_user_1['password'],
+        })
+    assert response.status_code == 200
+    resp_body = response.json()
+    assert response.status_code == 200 and 'access_token' in resp_body
+    token = resp_body['access_token']
+    return token
+
+@pytest.fixture(scope='module', autouse=False)
+def token_user_2(test_user_2):
+    response = requests.post(LOGIN_URL, json={
+            'username': test_user_2['username'],
+            'password': test_user_2['password'],
+        })
+    assert response.status_code == 200
+    resp_body = response.json()
+    assert response.status_code == 200 and 'access_token' in resp_body
+    token = resp_body['access_token']
+    return token
+
+@pytest.fixture(scope='function', autouse=False)
+def random_task(test_user_1):
+    with app.app_context():
+        random_task = TaskModel.query.filter_by(user_id=test_user_1['id']).order_by(func.random()).limit(1).all()[0]
+        return random_task.__dict__
+
 class TestTasksAPI:
 
-    def test_get_list_of_tasks(self, test_user_1):
+    def test_get_list_of_tasks(self, token_user_1, test_user_1):
         """
             TEST GET Request to get all Tasks in DB
             belonging to Logged User
         """
-        response = requests.post(LOGIN_URL, json={
-            'username': test_user_1['username'],
-            'password': test_user_1['password'],
-        })
-        assert response.status_code == 200
-
-        resp_body = response.json()
-        assert response.status_code == 200 and 'access_token' in resp_body
-        token = resp_body['access_token']
+        
         response = requests.get('{}/tasks/'.format(ROOT_URL),
             headers={
-                'Authorization': 'Bearer {}'.format(token)
+                'Authorization': 'Bearer {}'.format(token_user_1)
             })
         nbr_of_tasks_in_response = len(response.json())
         assert nbr_of_tasks_in_response > 0
         with app.app_context():
             nbr_of_tasks_in_db = len(TaskModel.query.filter_by(user_id=test_user_1['id']).all())
             assert nbr_of_tasks_in_response == nbr_of_tasks_in_db
-    '''
-    def test_get_specific_task(self):
+
+    def test_get_specific_task(self, token_user_1, random_task):
         """
-            TEST Get A Specific Task Details from DB
+            TEST Get A Specific Task Details from DB belonging to logged user
         """
         with app.app_context():
-            random_task = TaskModel.query.all()[0].__dict__
-            response = requests.get('{}/tasks/{}/'.format(ROOT_URL, random_task['id']))
+            response = requests.get(
+                '{}/tasks/{}/'.format(ROOT_URL, random_task['id']),
+                headers={'Authorization': 'Bearer {}'.format(token_user_1)}
+                )
             assert response.status_code == 200, 'Should get a 200 Code Response. Got a %s instead' % response.status_code
             assert response.json()['id'] == random_task['id'], 'Expecting ID of retrieved Task to be the same as the one requested'
             assert response.json()['title'] == random_task['title'], 'Expecting title of retrieved Task to be the same as the one in DB'
 
-    def test_post_new_task(self):
+    def test_post_new_task(self, token_user_1, test_user_1):
         """
             TEST POST Request to add a new Task in DB
         """
+        new_task = { 'title': 'New Task Test'}
         with app.app_context():
-            nbr_of_tasks_in_db = len(TaskModel.query.all())
-            response = requests.post('{}/tasks/'.format(ROOT_URL), json={
-                'title': 'New Task Test'
-            })
+            nbr_of_tasks_in_db = len(TaskModel.query.filter_by(user_id=test_user_1['id']).all())
+            response = requests.post(
+                '{}/tasks/'.format(ROOT_URL),
+                headers={'Authorization': 'Bearer {}'.format(token_user_1)},
+                json={ 'title': new_task['title'] }
+                )
             assert response.status_code == 201, 'Should get a 201 Code Response'
-            assert nbr_of_tasks_in_db + 1 == len(TaskModel.query.all()) , 'Expecting number of tasks in DB to increase by one'
-            assert response.json()['title'] == 'New Task Test', 'Expecting title of returned object to match title sent in post request'
-    
-    def test_patch_task(self):
+            assert nbr_of_tasks_in_db + 1 == len(TaskModel.query.filter_by(user_id=test_user_1['id']).all()) , 'Expecting number of tasks in DB to increase by one'
+            assert response.json()['title'] == new_task['title'], 'Expecting title of returned object to match title sent in post request'
+            assert response.json()['user_id'] == test_user_1['id'], 'Expecting title of returned object to match title sent in post request'
+
+    def test_patch_task(self, token_user_1, random_task):
         """
             TEST PATCH Request to update a Task in DB
         """
         with app.app_context():
-            random_task = TaskModel.query.all()[0].__dict__
             new_title = 'Updated Title of Task'
-            response = requests.patch('{}/tasks/{}/'.format(ROOT_URL, random_task['id']), json={
-                'title': new_title
-            })
+            response = requests.patch(
+                '{}/tasks/{}/'.format(ROOT_URL, random_task['id']),
+                headers={'Authorization': 'Bearer {}'.format(token_user_1)},
+                json={ 'title': new_title })
             assert response.status_code == 200, 'Should get a 200 Code Response'
             updated_task = TaskModel.query.get(random_task['id']).__dict__
             assert updated_task['title'] == new_title, 'Expected to update the new title in DB'
 
-    def test_delete_task(self):
+    def test_delete_task(self, token_user_1, random_task):
         """
             TEST DELETE Request to delete a Task from DB
         """
         with app.app_context():
-            nbr_of_tasks_in_db = len(TaskModel.query.all())
-            random_task = TaskModel.query.all()[0].__dict__
-            response = requests.delete('{}/tasks/{}/'.format(ROOT_URL, random_task['id']))
+            user_id = random_task['user_id']
+            pre_nbr_of_tasks_in_db = len(TaskModel.query.filter_by(user_id=user_id).all())
+            response = requests.delete(
+                '{}/tasks/{}/'.format(ROOT_URL, random_task['id']),
+                headers={'Authorization': 'Bearer {}'.format(token_user_1)},
+                )
             assert response.status_code == 204, 'Should get a 201 Code Response'
-            assert nbr_of_tasks_in_db - 1 == len(TaskModel.query.all()) , 'Expecting number of tasks in DB to decrease by one'
-    '''
+            nbr_of_tasks_in_db = len(TaskModel.query.filter_by(user_id=user_id).all())
+            assert nbr_of_tasks_in_db  == pre_nbr_of_tasks_in_db - 1, 'Expecting number of tasks in DB to decrease by one'
